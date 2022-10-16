bb71ee8d6fbbc791e3971afb66394c29250a6c38
ABC <1395813044@qq.com>
2022-10-16 17:04:25 +0800

    14-3-7

--- hotel-manager/src/main/java/com/wang/controller/PageController.java
+++ hotel-manager/src/main/java/com/wang/controller/PageController.java
@@ -9,52 +9,47 @@ public class PageController {
 
     /**
      * 去到部门管理页面
-     *
      * @return
      */
     @RequestMapping("/toDeptManager.html")
-    public String toDeptManager() {
+    public String toDeptManager(){
         return "dept/deptManager";
     }
 
     /**
      * 去到角色管理页面
-     *
      * @return
      */
     @RequestMapping("/toRoleManager.html")
-    public String toRoleManager() {
+    public String toRoleManager(){
         return "role/roleManager";
     }
 
     /**
      * 去到用户管理页面
-     *
      * @return
      */
     @RequestMapping("/toUserManager.html")
-    public String toUserManager() {
+    public String toUserManager(){
         return "user/userManager";
     }
 
     /**
      * 去到权限管理页面
-     *
      * @return
      */
     @RequestMapping("/toPermissionManager.html")
-    public String toPermissionManager() {
+    public String toPermissionManager(){
 
         return "permission/permissionManager";
     }
 
     /**
      * 去到楼层管理页面
-     *
      * @return
      */
     @RequestMapping("/toFloorManager.html")
-    public String toFloorManager() {
+    public String toFloorManager(){
         return "floor/floorManager";
     }
 
@@ -62,36 +57,34 @@ public class PageController {
      * 去到房型管理页面
      */
     @RequestMapping("/toRoomTypeManager.html")
-    public String toRoomTypeManager() {
+    public String toRoomTypeManager(){
         return "roomType/roomTypeManager";
     }
 
     /**
      * 去到房间管理页面
-     *
      * @return
      */
     @RequestMapping("/toRoomManager.html")
-    public String toRoomManager() {
+    public String toRoomManager(){
         return "room/roomManager";
+    }
+    /**
+     * 去到年度营业额报表统计分析页面
+     * @return
+     */
+    @RequestMapping("/toYearTotalPriceManager.html")
+    public String toYearTotalPriceManager(){
+        return "charts/yearTotalPriceCharts";
     }
 
     /**
-     * 去到预订管理页面
-     *
+     * 去到月营业额报表统计分析页面
      * @return
      */
-    @RequestMapping("/toOrdersManager.html")
-    public String toOrdersManager() {
-        return "orders/ordersManager";
+    @RequestMapping("/toYearOfMonthCharts.html")
+    public String toYearOfMonthCharts(){
+        return "charts/yearOfMonthCharts";
     }
-    /**
-     * 去到入住管理页面
-     *
-     * @return
-     */
-    @RequestMapping("/toCheckinManager.html")
-    public String toCheckinManager() {
-        return "checkin/checkinManager";
-    }
+
 }
